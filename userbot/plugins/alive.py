8f1ece52565e3b40e18d52f2619846821509802e
Ahmad99Ahmad <60040896+Ahmad99Ahmad@users.noreply.github.com>
2020-04-18 21:43:10 +0300

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,18 +1,19 @@
-"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
-import asyncio
-from telethon import events
-from telethon.tl.types import ChannelParticipantsAdmins
-from platform import uname
-from userbot import ALIVE_NAME
-from userbot.utils import admin_cmd
-
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @vhhhhh"
-
-@command(outgoing=True, pattern="^.alive$")
-async def amireallyalive(alive):
-    """ For .alive command, check if the bot is running.  """
-    await alive.edit("`Currently Alive!` **ψ(｀∇´)ψ**\n\n"
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\n`"
-                     "`Bot created by:` [hassanmuaef](tg://user?id=665877797), @vhhhhh\n"
-                     f"`My peru owner`: {DEFAULTUSER}\n\n"
-                     "https://github.com/PaYtHoNMeMbEr/PaYtHoNMeMbEr")
+"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you.""" 
+import asyncio 
+from telethon import events 
+from telethon.tl.types import ChannelParticipantsAdmins 
+from platform import uname 
+from userbot import ALIVE_NAME 
+from userbot.utils import admin_cmd 
+ 
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @XtraTgBot" 
+ 
+@command(outgoing=True, pattern="^.alive$") 
+async def amireallyalive(alive): 
+    """ For .alive command, check if the bot is running.  """ 
+    await alive.edit("**`YO BRO JABTAK YE KHEL KHATAM NHI HOTA APUN IDHARICH HAI ψ(｀∇´)ψ`**\n\n" 
+                     "`Telethon version: 6.9.0\nPython: 3.7.3\n تم التنصيب بواسطه :@HHMHHH`/n" 
+                     "`Bot created by:` [𓆩 𝙰𝙷𝙼𝙰𝙳 𝙱𝙰𝚁𝙰𝙽 𓆪](tg://user?id=801023241)\n" 
+                     "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`" 
+                     f"`My peru owner`: {DEFAULTUSER}\n" 
+                     "[اشترك بقناة السورس لرؤيه كل ما هو جديد](https://t.me/joinchat/AAAAAEwLzFqiU7iYmuggWQ)")
